37f83bc120ea336c5ec0a96fcd2e06fb0cac28f4
and1helicopter <and1helicopter@mail.ru>
2016-03-20 23:30:07 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+def application(environ, start_response):
+  status = '200 OK'
+  headers = [('Content-Type', 'text/plain')]
+  start_response(status, headers)
+  body="\r\n".join(environ['QUERY_STRING'].split('&'))
+  return [body]
