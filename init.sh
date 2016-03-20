0f5819bd58afd91aa3fd885a6a9f109c75285484
and1helicopter <and1helicopter@mail.ru>
2016-03-20 23:14:58 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #gunicorn conf
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/wsgi.example  
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/wsgi.example  
 sudo /etc/init.d/gunicorn restart 
 
 
