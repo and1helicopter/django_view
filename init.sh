317f13673e55f7d514546febd008837fc8898ce2
and1helicopter <and1helicopter@mail.ru>
2016-03-20 21:19:20 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart 
 cd /home/box/web/etc/
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
 
 
 
