f7798b43d0d5265d62aceac2b41a66dbd6c3c855
and1helicopter <and1helicopter@mail.ru>
2016-03-20 20:56:15 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,7 +1,7 @@
 def application(environ, start_response):
-
   status = '200 OK'
   headers = [('Content-Type', 'text/plain')]
   start_response(status, headers)
   body="\r\n".join(environ['QUERY_STRING'].split('&'))
   return [body]
+
