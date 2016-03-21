d322a3963a6cb3d8afb7d362e332113e1d147d0f
and1helicopter <and1helicopter@mail.ru>
2016-03-21 23:12:03 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,14 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-     url(r'^$', 'test', name='home'),
-     url(r'^login/.*', 'test', name='login'),
-     url(r'^signup/.*', 'test', name='signup'),
-     url(r'^question/.*', 'test', name='question'),
-     url(r'^question/(?P<id>[0-9]+)/$', 'ask.views.home', name='home'),
-     url(r'^ask/.*', 'test', name='ask'),
-     url(r'^popular/.*', 'test', name='popular'),
-     url(r'^new/.*', 'test', name='new'),
-    #url(r'^$', 'ask.views.home', name='home'),
-
+     url(r'^$', 'qa.views.test', name='home'),
+     url(r'^login/.*', 'qa.views.test', name='login'),
+     url(r'^signup/.*', 'qa.views.test', name='signup'),
+     url(r'^question/(?P<id>[0-9]+)/$', 'qa.views.test', name='question'),
+     url(r'^ask/.*', 'qa.views.test', name='ask'),
+     url(r'^popular/.*', 'qa.views.test', name='popular'),
+     url(r'^new/.*', 'qa.views.test', name='new'),
 )
