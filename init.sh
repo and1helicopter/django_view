cbd357173141915e9272493760d9957f2cb8ea6b
and1helicopter <and1helicopter@mail.ru>
2016-03-21 23:34:57 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ sudo /etc/init.d/nginx restart
 #gunicorn conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/wsgi.example  
 sudo /etc/init.d/gunicorn restart 
+cd ask/ask/
+gunicorn -b 0.0.0.0:8000 wsgi
 
 
 
