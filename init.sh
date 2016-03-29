bd0dba35ed2a397f7baab68a37f90033fd4e7bb1
and1helicopter <and1helicopter@mail.ru>
2016-03-29 19:41:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,9 +5,17 @@ sudo /etc/init.d/nginx restart
 #gunicorn conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/wsgi.example  
 sudo /etc/init.d/gunicorn restart 
-cd ask/ask/
-gunicorn -b 0.0.0.0:8000 wsgi
 
+#database conf
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'and1helicopter' IDENTIFIED BY 'and1helicopter'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'and1helicopter'"
+
+python /home/box/web/ask/manage.py syncdb
+
+#gunicorn
+cd /home/box/webask/ask/
+gunicorn -b 0.0.0.0:8000 wsgi
 
 
 
